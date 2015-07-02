f60a837e42bce76c76f55998f5130d44d31ce795
Carsten Schoenert <c.schoenert@t-online.de>
2015-07-02 20:18:31 +0200

    RPi2: adding specific memory environment
    The RPi2 probably needs different memory addresses for scripts and the
    FDT binary.

--- include/configs/rpi_2.h
+++ include/configs/rpi_2.h
@@ -12,4 +12,18 @@
 
 #include "rpi-common.h"
 
+#undef ENV_MEM_LAYOUT_SETTINGS
+#define ENV_MEM_LAYOUT_SETTINGS \
+	"scriptaddr=0x02000000\0" \
+	"pxefile_addr_r=0x00100000\0" \
+	"kernel_addr_r=0x01000000\0" \
+	"fdt_addr_r=0x00000100\0" \
+	"ramdisk_addr_r=0x02100000\0" \
+
+#undef BOOT_TARGET_DEVICES
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(USB, usb, 0) \
+	func(DHCP, dhcp, na)
+
 #endif
